1e9e1dd15bfdd69867a800dc4600ce8723676f30
Stanley <infamous_godhand@yahoo.com>
2020-04-12 09:46:15 +1000

    trying withRedux wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import App from "next/app";
 import { Provider } from "react-redux";
+import withRedux from "next-redux-wrapper";
+import store from "../store/setupStore";
 import React from "react";
 
 const MyApp = ({ Component, pageProps }) => {
@@ -9,5 +11,15 @@ const MyApp = ({ Component, pageProps }) => {
     </Provider>
   );
 };
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  const pageProps = Component.getInitialProps
+    ? await Component.getInitialProps(ctx)
+    : {};
 
-export default MyApp;
+  //Anything returned here can be access by the client
+  return { pageProps: pageProps };
+};
+const makeStore = () => store;
+
+//withRedux wrapper that passes the store to the App Component
+export default withRedux(makeStore)(MyApp);
