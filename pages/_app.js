0bf8b7a9bb44cd560fec6a7eb871ab4acce20c69
Stanley <infamous_godhand@yahoo.com>
2020-04-12 10:15:23 +1000

    removed weird ";"

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import React from "react";
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Provider>
   );
 };
